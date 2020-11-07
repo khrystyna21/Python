cb04b8cf08256dcd26eb28f52f6320fda8cbcad9
Khrystyna Tkachuk <khrystynatkachuk@Khrystynas-MacBook-Air.local>
2020-11-07 11:37:18 +0200

    updated 4.3

--- lesson4/4.3.py
+++ lesson4/4.3.py
@@ -1,6 +1,6 @@
 
 
-from random import shuffle
+'''from random import shuffle
 
 word=input('Write the word: ')
 word_as_list = list(word)
@@ -9,7 +9,25 @@ ind = 0
 while ind < 5:
     shuffle(word_as_list)
     print(''.join(word_as_list))
-    ind +=1
+    ind +=1'''
+
+import random
+
+word = input('Enter your word: ')
+
+str_to_generate = 5
+
+while str_to_generate > 0:
+    word_copy = word
+    new_random_word = ''
+    while True:
+        random_char = random.choice(word_copy)
+        new_random_word += random_char
+        word_copy = word_copy.replace(random_char, '',1 )
+        if not word_copy:
+            print(new_random_word)
+            break
+    str_to_generate -= 1
 
 
 
